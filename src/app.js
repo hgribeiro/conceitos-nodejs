fd2487654dade596b29758ae741dff6285b8f5f5
Hugo Ribeiro <hgribeiro@live.com>
2020-04-07 11:18:45 -0300

    Desafio1 nodejs finalizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,75 @@ app.use(cors());
 
 const repositories = [];
 
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);  
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+ const {title, url, techs } = request.body;
+
+ const repositorie = { id: uuid(), title, url, techs, likes: 0 };
+
+ repositories.push(repositorie);
+ 
+ return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs } = request.body;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({  error: 'Project not found.' });
+  }
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes,
+  }
+  
+  repositories[repositorieIndex] =repositorie;
+
+  return response.json(repositorie)
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const {id} = req.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return res.status(400).json({ error: 'Project not found.'});
+  }
+  
+  
+  repositories.splice(repositorieIndex, 1);
+
+  return res.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+  const {id} = request.params;
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
 
+if (repositorieIndex < 0) {
+   return response.status(400).json({ error: 'Project not found.'})
+}
+
+const repositorie = {
+  id,
+  title: repositories[repositorieIndex].title,
+  url: repositories[repositorieIndex].url,
+  techs: repositories[repositorieIndex].techs,
+  likes: repositories[repositorieIndex].likes + 1,
+} 
+
+repositories[repositorieIndex] =repositorie;
+  return response.json(repositorie); 
+});
 module.exports = app;
